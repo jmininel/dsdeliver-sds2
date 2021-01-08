271ebb4485f442263665c365412519b02a663315
jmininel <ju.mininel@hotmail.com>
2021-01-07 21:35:36 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,8 +1,10 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
 function Home() {
     return (
+       <> 
         <div className="home-container">
           <div className="home-content">
             <div className="home-actions">
@@ -22,8 +24,13 @@ function Home() {
                <MainImage />
             </div>
           </div>
-
         </div>
+       <Footer />
+
+      
+
+       </>
+
     )
 }
 
